5e7ba48ef00648addc9bf7a90d7543ba64ffd3b1
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-19 06:32:56 +0000

    automatically backed up by learn

---
+++ artist.rb
@@ -0,0 +1,12 @@
+class Artist < ActiveRecord::Base
+
+
+    def self.create_table
+    end
+end
+    #
+    # # Artist.connection
+    # # a = Artist.new
+    # Artist.create = (name: , genre: , age: , hometown:)
+    # Artist.find_by(name)
+    #   # Artist.save
